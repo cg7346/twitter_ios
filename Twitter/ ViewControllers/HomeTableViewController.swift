e8ca1ea04ccbb01f0d8c8821c473fe998e8ea8f5
cg7346 <cg7346@rit.edu>
2020-10-06 16:27:49 -0400

    fixing bug

--- Twitter/ ViewControllers/HomeTableViewController.swift
+++ Twitter/ ViewControllers/HomeTableViewController.swift
@@ -5,57 +5,32 @@
 //  Created by Celeste Gambardella on 10/5/20.
 //  Copyright © 2020 Dan. All rights reserved.
 //
-
 import UIKit
-
 class HomeTableViewController: UITableViewController {
+     var currentDate = Date()
+     var tweetArray = [NSDictionary]()
+     var tweetCount: Int!
+     var numberOfTweets: Int!
 
-    var tweetArray = [NSDictionary]()
-    var tweetCount: Int!
-    var numberOfTweets: Int!
-    var currentDate = Date()
-    
-    let myRefreshControl = UIRefreshControl()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loadTweets()
-        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
-        tableView.refreshControl = myRefreshControl
-        loadTweet()
-        
-        // Uncomment the following line to preserve selection between presentations
+     let myRefreshControl = UIRefreshControl()
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         loadTweets()
+         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+         tableView.refreshControl = myRefreshControl
+
+         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
+         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+     }
 
-    func loadTweet() {
-        
-        let homeUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let params = ["count": 10]
-        
-        TwitterAPICaller.client?.getDictionariesRequest(url: homeUrl, parameters: params, success: { (tweets: [NSDictionary]) in
-            
-            self.tweetArray.removeAll()
-            for tweet in tweets {
-                self.tweetArray.append(tweet)
-            }
-            
-            self.tableView.reloadData()
-            
-        }, failure: { (Error) in
-            print("Could not recieve tweets! Oh no!! ")
-        })
-    }
-    func getTimeElasped(date: String) -> String {
+     func getTimeElasped(date: String) -> String {
 
-        let dateFormat = "E, MMM d HH:mm:ss Z yyyy"
-
+         let dateFormat = "E, MMM d HH:mm:ss Z yyyy"
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = dateFormat
-
         let currentDate = Date()
         let creationTime = dateFormatter.date(from: date)
         
@@ -67,69 +42,68 @@ class HomeTableViewController: UITableViewController {
         let hours = "\(difference.hour ?? 0)h"
         let days = "\(difference.day ?? 0)d"
         let months = "\(difference.month ?? 0)M"
-
         if difference.month ?? 0 > 0 { return months }
         if difference.day ?? 0 > 0 { return days }
         if difference.hour ?? 0  > 0 { return hours }
         if difference.minute ?? 0  > 0 { return minutes }
         if difference.second ?? 0  > 0 { return seconds }
-        return ""
-    
-    }
-    
-    @objc func loadTweets() {
-        
-        numberOfTweets = 20
-        
-        let homeUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let params = ["count": numberOfTweets]
-        
-        TwitterAPICaller.client?.getDictionariesRequest(url: homeUrl, parameters: params, success: { (tweets: [NSDictionary]) in
-            
-            self.tweetArray.removeAll()
-            for tweet in tweets {
-                self.tweetArray.append(tweet)
-            }
-            
-            self.tableView.reloadData()
-            self.myRefreshControl.endRefreshing()
-            
-        }, failure: { (Error) in
-            print("Could not recieve tweets! Oh no!! ")
-        })
-    }
-    
-    func loadMoreTweets() {
-        
-        let homeUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        numberOfTweets += 20
-        
-        let params = ["count": numberOfTweets]
-            
-        TwitterAPICaller.client?.getDictionariesRequest(url: homeUrl, parameters: params, success: { (tweets: [NSDictionary]) in
-            
-            self.tweetArray.removeAll()
-            for tweet in tweets {
-                self.tweetArray.append(tweet)
-            }
-            
-            self.tableView.reloadData()
-            self.myRefreshControl.endRefreshing()
-            
-        }, failure: { (Error) in
-            print("Could not recieve tweets! Oh no!! ")
-        })
-    }
-    
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row + 1 == tweetArray.count {
-            loadMoreTweets()
-        }
-    }
-    
-    @IBAction func onLogout(_ sender: Any) {
-        TwitterAPICaller.client?.logout()
-        self.dismiss(animated: true, completion: nil)
+         return ""
+
+     }
+
+     @objc func loadTweets() {
+
+         numberOfTweets = 20
+
+         let homeUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+         let params = ["count": numberOfTweets]
+
+         TwitterAPICaller.client?.getDictionariesRequest(url: homeUrl, parameters: params, success: { (tweets: [NSDictionary]) in
+
+             self.tweetArray.removeAll()
+             for tweet in tweets {
+                 self.tweetArray.append(tweet)
+             }
+
+             self.tableView.reloadData()
+             self.myRefreshControl.endRefreshing()
+
+         }, failure: { (Error) in
+             print("Could not recieve tweets! Oh no!! ")
+         })
+     }
+
+     func loadMoreTweets() {
+
+         let homeUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+         numberOfTweets += 20
+
+         let params = ["count": numberOfTweets]
+
+         TwitterAPICaller.client?.getDictionariesRequest(url: homeUrl, parameters: params, success: { (tweets: [NSDictionary]) in
+
+             self.tweetArray.removeAll()
+             for tweet in tweets {
+                 self.tweetArray.append(tweet)
+             }
+
+             self.tableView.reloadData()
+             self.myRefreshControl.endRefreshing()
+
+         }, failure: { (Error) in
+             print("Could not recieve tweets! Oh no!! ")
+         })
+     }
+
+     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+         if indexPath.row + 1 == tweetArray.count {
+             loadMoreTweets()
+         }
+     }
+
+     @IBAction func onLogout(_ sender: Any) {
+         TwitterAPICaller.client?.logout()
+         self.dismiss(animated: true, completion: nil)
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
     }
     
@@ -159,32 +133,25 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.layer.masksToBounds = true
             cell.profileImageView.layer.cornerRadius = cell.profileImageView.bounds.width / 2
         }
-
         return cell
     }
     
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return tweetArray.count
     }
-
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
         // Configure the cell...
-
         return cell
     }
     */
-
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -192,7 +159,6 @@ class HomeTableViewController: UITableViewController {
         return true
     }
     */
-
     /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -204,14 +170,11 @@ class HomeTableViewController: UITableViewController {
         }
     }
     */
-
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
     }
     */
-
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
@@ -219,15 +182,12 @@ class HomeTableViewController: UITableViewController {
         return true
     }
     */
-
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     */
-
 }
